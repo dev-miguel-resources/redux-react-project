cf9a3b28c30c61fda8bf450dc8020019e04f8cac
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-01-13 09:25:00 -0300

    working in configuracion webpack-babel

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,14 +3,23 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "../assets/styles/App.scss";
 import Characters from "../containers/Characters";
 import Layout from "../components/Layout";
+import Loader from "../components/Loader";
 const Favorites = lazy(() => import("../containers/Favorites"));
 
 const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-        <Route exact path="/favorites" component={Favorites} />
-        <Route exact path="/" component={Characters} />
+        <Suspense
+          fallback={
+            <div>
+              <Loader />
+            </div>
+          }
+        >
+          <Route exact path="/favorites" component={Favorites} />
+          <Route exact path="/" component={Characters} />
+        </Suspense>
       </Switch>
     </Layout>
   </BrowserRouter>
